3e90f1731fc86cd973655b33be02db96602a84e0
frame-of-reference <info@frame-of-reference.com>
2021-12-31 19:23:17 -0700

    Added d3 import

--- react-frontend/src/views/HomeView.jsx
+++ react-frontend/src/views/HomeView.jsx
@@ -1,37 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-
-
-
-
-
-
-const randNumbers = [
-  {
-    id: 0,
-    title: 'Lorem1 IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum ',
-    description: 'ipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sit',
-    rating: 10,
-  },
-  {
-    id: 1,
-    title: 'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum ',
-    description: 'ipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sit',
-    rating: 4,
-  },
-  {
-    id: 2,
-    title: 'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum ',
-    description: 'ipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sit',
-    rating: 3,
-  },
-  {
-    id: 3,
-    title: 'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum ',
-    description: 'ipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sitipsum dolor sit',
-    rating: 2,
-  }
-]
+import * as d3 from 'd3';
 
 
 function ListElement() {
@@ -39,11 +8,12 @@ function ListElement() {
   const [showNav, toggleNav] = useState(false);
   const [count, incrementCount] = useState(0);
 
+  const div = d3.selectAll('div')
 
   return (
-      <React.Fragment>
+    <div>
 
-      </React.Fragment>
+    </div>
   )
 }
 
