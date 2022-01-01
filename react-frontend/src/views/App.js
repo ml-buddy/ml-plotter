246788d0a9f4935e18c92b5a434b184e954bc6e7
frame-of-reference <admin@studium.dev>
2022-01-01 01:06:01 -0700

    initial setup

---
+++ react-frontend/src/views/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HomeView from "@views/HomeView";
+import ArticleView from "@views/ArticleView";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '@styles/main.css'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path='/articles' element={<ArticleView />} />
+
+          <Route path='/' element={<HomeView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/views/HomeView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeView() {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+export default HomeView;
